5b49373b6a987f2143d64f1797008bb099b37f2f
Jose Angel <jafb19@outlook.com>
2021-11-04 01:01:27 +0100

    dbconfig y operaciones

---
+++ config/operaciones_bd.php
@@ -0,0 +1,9 @@
+<?php
+
+    class operaciones{
+        public function guardarRecords(){
+            if(isset($_POST['guardar'])){
+                echo 'Funciona';
+            }
+        }
+    }
